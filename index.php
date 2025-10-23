3e387d828cb4b4e0aef0fc883c8132d4a1f5ec3d
Juliana <jujugomesjuju456@gmail.com>
2025-10-23 13:59:23 -0300

    Contiuação do arquivo da página inicial

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('db_connection.php'); ?>
 <!DOCTYPE html>
  <html lang = "pt-br">
     <head>
@@ -11,5 +12,29 @@
         <input type = "text" name = "title" placeholder = "Nova Tarefa..." required>
         <button type = "submit">Adicionar</button>
 </fom>
+ 
+<hr>
+
+<ul>
+    <?php
+    $sql = "SELECT * FROM tasks ORDER BY id DESC";
+    $result = $conn -> query($sql);
+
+    if ($result->num_rows > 0){
+      while ($row = $result->fetch_assoc()) {
+        echo "<li>";
+        echo $row['status'] == 'concluída' ? "<s> {$row['title']}</s>" : $row['title'];
+        echo "
+          <a hret= 'update_task.php?id={$row['id']}'>V</a>
+                    <a hret= 'update_task.php?id={$row['id']}'>L</a>";
+                    echo "</li>";
+                    
+      }
+    
+    } else {
+        echo "<p>Nenhuma tarefa cadastrada.</p>";
+    }
+        ?>
+    </ul>
 </body>
 </html>
