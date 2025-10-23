38d60bfd3c629d4d5fa61ffa20472eb097242ae0
Juliana <jujugomesjuju456@gmail.com>
2025-10-23 10:29:03 -0300

    Criação da página inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+ <html lang = "pt-br">
+    <head>
+        <meta charset = "UTF-8">
+        <title>To Do list</title>
+        <link rel ="stylesheet" href ="style.css">
+</head>
+<body> 
+    <h1>Minha To do list</h1>
+    <form action = "add_task.php" metholder = "POST">
+        <input type = "text" name = "title" placeholder = "Nova Tarefa..." required>
+        <button type = "submit">Adicionar</button>
+</fom>
+</body>
+</html>
